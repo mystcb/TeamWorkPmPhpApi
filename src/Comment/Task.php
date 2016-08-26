70d71f0dc14a509e1c3ccf743e618c30e50f3e0a
Colin Barker <colin@faereal.net>
2016-08-26 12:41:56 +0100

    Added code for Comment\Task retreval

--- src/Comment/Task.php
+++ src/Comment/Task.php
@@ -3,4 +3,24 @@
 class Task extends Model
 {
     protected $resource = 'todo_items';
+
+    protected function init()
+    {
+        $this->action = 'comments';
+    }
+
+    /**
+     * Retrieve Single comment
+     * GET /comments/#{id}.xml
+     *
+     * Retrieves the comment corresponding to the submitted integer ID.
+     */
+    public function get($id, $params  = NULL)
+    {
+        $id = (int) $id;
+        if ($id <= 0) {
+            throw new Exception('Invalid param id');
+        }
+        return $this->rest->get("$this->action/$id");
+    }
 }
